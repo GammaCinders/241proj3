e6c508a61f2b6532830958e0f0dfd0c339004d73
GammaCinders <talsmake@mail.edu.gvsu>
2022-04-06 12:44:18 -0400

    Worked on backup.sh, the BASH script Added .gitignore to ignore debug/testing files

---
+++ backup.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "give me some arguments man!"
+	exit
+fi
+
+backupPath="${HOME}/backup"
+if ! [ -d "${backupPath}" ]
+then
+	mkdir ${backupPath}
+fi
+
+for arg in $*
+do
+	if [ $(echo $arg | cut -b-1) = "-" ]
+	then
+		case $arg in
+
+			-l)
+				echo
+				echo "Files in ~/backup:" 
+				ls "${backupPath}"
+				echo
+				;;
+			-c)
+				echo
+				echo "Number of files and directories in ~/backup $(ls $backupPath | wc -w)"
+				echo "Disk usage of files and directories in ~/backup $(du -b $backupPath)"
+				echo
+				;;
+			--help)
+				echo
+				echo "HELP : backup copies provided files to ~/backup with options"
+				echo "Usage : "
+				echo "	backup [FILES...] [OPTIONS...]"
+				echo "OPTIONS are :"
+				echo "	-c, displays number of files and directories in ~/backup as well as disk usage of them"
+				echo "	-l, displays all files in ~/backup"
+				echo "	--help, displays this text"	
+				echo
+				;;
+			*)
+				echo "$arg is an invalid arg"
+				;;
+		esac
+	else
+		if [ -e $arg ]
+		then
+			cp -r $arg $backupPath
+		else
+			echo "$arg does not exist"
+		fi
+	fi	
+done
